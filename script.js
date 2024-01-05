6def553e7c9142b3c7fef320100952a9500d1b33
Dev-Gitanjali <jgitanjali592@gmail.com>
2024-01-05 13:17:33 +0530

    this is the @media it can help change the size

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const left = document.querySelector('.left')
 const right = document.querySelector('.right')
 const container = document.querySelector('.container')
 
+// this line for left split landing page  move
+
 left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
 left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
 
+// right side landing page can move
 right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
 right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
